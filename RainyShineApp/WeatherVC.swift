d444c49aca48b0f5a661710e5c3be3e2bee056e3
Christine Cunniff <christinecunniff@gmail.com>
2016-12-19 18:56:54 -0500

    Connected outlets for existing UI.

--- RainyShineApp/WeatherVC.swift
+++ RainyShineApp/WeatherVC.swift
@@ -3,6 +3,13 @@ import Alamofire
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
